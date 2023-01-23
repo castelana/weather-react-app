cc7e450ad37ae4ea0b4a7e529388eb21a6d7f1fb
Anastasiia Kichkimet <108686215+castelana@users.noreply.github.com>
2023-01-23 14:42:07 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was created by Anastasiia Kichkimet and is
+        <a
+          href="https://github.com/castelana/weather-react-app"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
