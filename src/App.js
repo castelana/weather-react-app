eb9b297f695cae03ce026376d98f53b9ce08a128
Anastasiia Kichkimet <108686215+castelana@users.noreply.github.com>
2023-01-24 16:29:27 +0100

    created a Weather component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import "./App.css";
-
+import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was created by Anastasiia Kichkimet and is
-        <a
-          href="https://github.com/castelana/weather-react-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was created by Anastasiia Kichkimet and is
+          <a
+            href="https://github.com/castelana/weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
